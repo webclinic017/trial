c36736768bc45244ce243e305df1eb84e7ac0032
Utkarsh Gupta <88526325+Utkarshhh20@users.noreply.github.com>
2022-05-22 23:41:32 +0400

    Update trial.py

--- trial.py
+++ trial.py
@@ -7,6 +7,40 @@ import matplotlib.pyplot as plt
 import matplotlib
 from rsi import RSIStrategy
 st.set_option('deprecation.showPyplotGlobalUse', False)
+title="""
+    <style>
+    .stockify {
+    text-align: center;
+    margin-top: -100px;
+    margin-left: 67px;
+    font-size: 40px;
+    font-family: Arial, Helvetica, sans-serif;
+    letter-spacing: 2px;
+    }
+    </style>
+    <body>
+    <p1 class='stockify'>Stockify</p1>
+    </body>
+    """
+st.sidebar.markdown(title, unsafe_allow_html=True)
+st.sidebar.write('_______________________')
+title_alignment2="""
+    <style>
+    .trial2 {
+    text-align: center;
+    margin-top: -30px;
+    margin-left: -13px;
+    font-size: 300px;
+    }
+    </style>
+    <body>
+    <h1 class='trial2'>Select your dashboard</h1>
+    </body>
+    """
+st.sidebar.markdown(title_alignment2, unsafe_allow_html=True)
+dashboard = st.sidebar.selectbox('', ('Home', 'Fundamental Analysis', 'Technical Indicators', 'Backtesting', 'Pattern Stocks'), 0)
+st.title(dashboard)
+st.write('___')
 st.write('hi')
 df = pdr.DataReader('AAPL', 'yahoo', start='2014-01-01', end='2017-01-01')
 st.dataframe(df)
