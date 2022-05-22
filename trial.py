729ff84c352a51061382f2c2eeeb1ea1db25aade
Utkarsh Gupta <88526325+Utkarshhh20@users.noreply.github.com>
2022-05-22 23:30:06 +0400

    Update trial.py

--- trial.py
+++ trial.py
@@ -3,6 +3,14 @@ import backtrader as bt
 import yfinance as yf
 import pandas as pd
 import pandas_datareader as pdr
+import matplotlib.pyplot as plt
+import pyplot
 st.write('hi')
 df = pdr.DataReader('AAPL', 'yahoo', start='2014-01-01', end='2017-01-01')
 st.dataframe(df)
+x=df['Close']
+fig=plt.title(symbol + ' Bollinger Bands')
+fig=plt.xlabel('Days')
+fig=plt.ylabel('Closing Prices')
+fig=plt.plot(x, label='Closing Prices')
+st.pyplot(fig)
