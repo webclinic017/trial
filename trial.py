5514d2c74048f2387ada7f8070fed756541f0e6e
Utkarsh Gupta <88526325+Utkarshhh20@users.noreply.github.com>
2022-05-22 23:32:10 +0400

    Update trial.py

--- trial.py
+++ trial.py
@@ -4,7 +4,6 @@ import yfinance as yf
 import pandas as pd
 import pandas_datareader as pdr
 import matplotlib.pyplot as plt
-import pyplot
 st.write('hi')
 df = pdr.DataReader('AAPL', 'yahoo', start='2014-01-01', end='2017-01-01')
 st.dataframe(df)
