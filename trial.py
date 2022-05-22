ad6a43388c6372617278de4b173e8bc2856c5a94
Utkarsh Gupta <88526325+Utkarshhh20@users.noreply.github.com>
2022-05-22 23:11:13 +0400

    Update trial.py

--- trial.py
+++ trial.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import backtrader as bt
-import yfinanceas yf
+import yfinanceas as yf
 st.write('hi')
 data = bt.feeds.PandasData(dataname=yf.download('AAPL', '2014-01-01', '2017-01-01'))
 st.dataframe(data)
