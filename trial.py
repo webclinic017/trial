960cf855ac27a09ecc9185ffba1485a4c853bcbd
Utkarsh Gupta <88526325+Utkarshhh20@users.noreply.github.com>
2022-05-22 23:37:04 +0400

    Update trial.py

--- trial.py
+++ trial.py
@@ -8,7 +8,7 @@ st.write('hi')
 df = pdr.DataReader('AAPL', 'yahoo', start='2014-01-01', end='2017-01-01')
 st.dataframe(df)
 x=df['Close']
-fig=plt.title(symbol + ' Bollinger Bands')
+fig=plt.title('AAPL' + ' Bollinger Bands')
 fig=plt.xlabel('Days')
 fig=plt.ylabel('Closing Prices')
 fig=plt.plot(x, label='Closing Prices')
