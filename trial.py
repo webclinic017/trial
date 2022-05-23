272f1159970e1e4eaa79be08e64b3b2f7bade85b
Utkarsh Gupta <88526325+Utkarshhh20@users.noreply.github.com>
2022-05-23 16:27:37 +0400

    Update trial.py

--- trial.py
+++ trial.py
@@ -41,6 +41,60 @@ st.sidebar.markdown(title_alignment2, unsafe_allow_html=True)
 dashboard = st.sidebar.selectbox('', ('Home', 'Fundamental Analysis', 'Technical Indicators', 'Backtesting', 'Pattern Stocks'), 0)
 st.title(dashboard)
 st.write('___')
+if dashboard=='Home':
+    file_ = open("C:/Users/Utki/Desktop/code/stock/Picture1.png", "rb")
+    contents = file_.read()
+    data_url = base64.b64encode(contents).decode("utf-8")
+    file_.close()
+    st.header('Welcome to Stockify')
+    title_alignment2='''
+    <style>
+    .trial {
+    width: 55px;
+    margin-top: -425px;
+    margin-left: 130px;
+    }
+    </style>
+    <body>
+    <img src="https://img.icons8.com/nolan/344/home-page.png" alt="House" class='trial'></img>
+    </body>
+    '''
+    image1='''
+    <style>
+    .stocks {
+    width: 50px;
+    margin-top: -165px;
+    margin-left: 320px;
+    }
+    </style>
+    <body>
+    <img src="https://img.icons8.com/external-xnimrodx-lineal-gradient-xnimrodx/344/external-stock-economy-xnimrodx-lineal-gradient-xnimrodx.png" alt="House" class='stocks'></img>
+    </body>'''
+    #st.markdown(f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">', unsafe_allow_html=True,)
+    st.markdown(title_alignment2, unsafe_allow_html=True)
+    st.markdown(image1, unsafe_allow_html=True)
+    st.subheader('What is stockify?')
+    st.write('Stockify is a web application developed on python using the streamlit library which aims to provide you with the tools necessary to make trading and investing much simpler.\n ')
+    st.write('''You can use this web app to do\n
+1. Fundamental Analysis\n
+2. Technical Analysis\n
+3. Backtesting''')
+if dashboard=='Fundamental Analysis':
+    s_fundament = st.sidebar.selectbox('What would you like to do?', ('Learn', 'Check fundamentals'), 0)
+    if s_fundament=='Learn':
+        st.subheader('What Is Fundamental Analysis?')
+        st.write('''• Fundamental analysis is a method of determining a stock's real or "fair market" value.\n
+• Fundamental analysts search for stocks that are currently trading at prices that are higher or lower than their real value.\n
+• If the fair market value is higher than the market price, the stock is deemed to be undervalued and a buy recommendation is given.\n
+• In contrast, technical analysts ignore the fundamentals in favor of studying the historical price trends of the stock.\n''')
+        st.subheader('Understanding Fundamental Analysis')
+        st.write('''All stock analysis tries to determine whether a security is correctly valued within the broader market. Fundamental analysis is usually done from a macro to micro perspective in order to identify securities that are not correctly priced by the market.\n
+For stocks, fundamental analysis uses revenues, earnings, future growth, return on equity, profit margins, and other data to determine a company's underlying value and potential for future growth. All of this data is available in a company's financial statements.\n''')
+        st.subheader('Financial Statements: Quantitative Fundamentals to Consider')
+        st.write('Financial statements are the medium by which a company discloses information concerning its financial performance. Followers of fundamental analysis use quantitative information gleaned from financial statements to make investment decisions. The three most important financial statements are income statements, balance sheets, and cash flow statements.')
+        st.write('')
+        st.write('''**Check the fundamentals of any stock by entering the ticker in the sidebar on the next page.**\n''')
+
 st.write('hi')
 df = pdr.DataReader('AAPL', 'yahoo', start='2014-01-01', end='2017-01-01')
 st.dataframe(df)
