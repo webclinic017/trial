fc0e4f7024e0702bd2859bc2c0a003d34b1c0547
Utkarsh Gupta <88526325+Utkarshhh20@users.noreply.github.com>
2022-05-24 19:09:29 +0400

    Update trial.py

--- trial.py
+++ trial.py
@@ -25,30 +25,17 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 today=date.today()
 def screener():
     qs.extend_pandas()
-    stock = qs.utils.download_returns('GLD', period="10y")
-    qs.reports.html(stock, title='PTL Investments', output='output/gld.html')
-    qs.reports.html(stock, "SPY", title="GLD vs. SPY", output='output/gld_vs_spy.html')
-    stock = qs.utils.download_returns('QQQ', period="10y")
-    qs.reports.html(stock, "SPY", title="QQQ vs. SPY", output='output/qqq_ovs_spy.html')
-    stock = qs.utils.download_returns('TQQQ', period="10y")
-    qs.reports.html(stock, "QQQ", title="TQQQ vs. QQQ", output='output/tqqq_vs_qqq.html')
-    tickers = {
-        'FB': 0.2,
-        'AAPL': 0.2, 
-        'AMZN': 0.2,
-        'MSFT': 0.2,
-        'GOOG': 0.2 
-    }
-    fmaga = portfolio = qs.utils.make_index(tickers)
-    report=qs.reports.html(fmaga, "qqq", output="output/fmaga_vs_qqq.html")
-    st.write(fmaga)
-    st.write(report)
-    stock.plot_monthly_heatmap(savefig='heatmap.png')
-    st.image('heatmap.png')
-    HtmlFile = open('output/gld.html', 'r', encoding='utf-8')
-    source_code = HtmlFile.read() 
-    print(source_code)
-    components.html(source_code)
+
+# fetch the daily returns for a stock
+    stock = qs.utils.download_returns('GOOG')
+
+# show sharpe ratio
+    stats=qs.stats.sharpe(stock)
+    st.write(stats)
+# or using extend_pandas() :)
+    stock.sharpe()
+    plot=qs.plots.snapshot(stock, title='Facebook Performance')
+    st.pyplot(plot)
 def backtestrsi():
     global strategy
     ticker=st.sidebar.text_input("Stock ticker", value="AAPL")
