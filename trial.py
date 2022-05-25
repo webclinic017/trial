ed2b2a7f18d43b3e1be59a630eb1d1a9dd6fdbee
Utkarsh Gupta <88526325+Utkarshhh20@users.noreply.github.com>
2022-05-25 19:12:31 +0400

    Update trial.py

--- trial.py
+++ trial.py
@@ -84,8 +84,8 @@ def backtestrsi():
     totalyear=year+(month/12)+(day/365)
     interactive_backends = ['Qt5Agg', 'MacOSX', 'TkAgg', 'ipympl', 'GTK3Agg', 'GTK3Cairo', 'nbAgg', 'Qt5Cairo','TkCairo']
     if iplot:
-    if 'ipykernel' in sys.modules:
-        matplotlib.use('nbagg')
+        if 'ipykernel' in sys.modules:
+            matplotlib.use('nbagg')
     cerebro.adddata(data)
 
     cerebro.addstrategy(RSIStrategy)
