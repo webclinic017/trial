fbcea44ddecd8f855d7faefaef339baf71126eae
Utkarsh Gupta <88526325+Utkarshhh20@users.noreply.github.com>
2022-05-27 15:34:23 +0400

    Update trial.py

--- trial.py
+++ trial.py
@@ -9,6 +9,8 @@ import numpy as np
 import warnings
 import quantstats as qs
 import streamlit.components.v1 as components
+from matplotlib import figure
+from matplotlib.backends.backend_agg import RendererAgg
 from matplotlib import warnings
 from matplotlib.dates import (HOURS_PER_DAY, MIN_PER_HOUR, SEC_PER_MIN)
 from urllib.request import urlopen, Request
@@ -16,6 +18,7 @@ from bs4 import BeautifulSoup as bs
 from rsi import RSIStrategy
 from goldencrossover import goldencrossover
 from datetime import date
+_lock = RendererAgg.lock
 #def fxn():
 #    warnings.warn("deprecated", DeprecationWarning)
 #with warnings.catch_warnings():
@@ -130,7 +133,8 @@ def backtestrsi():
     annual_return=round(annual_return,2)
     returns=str(returns)
     annual_return=str(annual_return)
-    figure = cerebro.plot()[0][0]
+    with lock:
+        figure = cerebro.plot()[0][0]
     st.pyplot(figure)
     st.write('')
     st.subheader(f"{ticker}'s total returns are {returns}% with a {annual_return}% APY")
